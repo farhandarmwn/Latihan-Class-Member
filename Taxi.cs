ee61eefddc334d9b7df3483327b27cb89c82b59d
Farhan3 <amrimuh16@gmail.com>
2019-03-27 20:26:02 -0700

    Latihan4

---
+++ Taxi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Taxi
+    {
+        // properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        // method
+        public void TaxiInfo() 
+            {
+                Console.WriteLine("DriverName: {0}", DriverName);
+                Console.WriteLine("OnDuty: {0}", OnDuty);
+                Console.WriteLine("NumPassenger: {0}", NumPassenger);
+            }
+
+            public void PickUpPassenger()
+            {
+                Console.WriteLine("{0} sedang menjemput Penumpang", DriverName);
+            }
+
+            public void DropOffPassenger()
+        { 
+            Console.WriteLine("{0} selesai mengantar Penumpang", DriverName);
+        }
+    }
+}
